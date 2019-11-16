3028446c9b96e34daed283fd0ddbfb58edf18927
Szymon <46461910+SzymonOlek@users.noreply.github.com>
2019-11-16 22:18:36 +0100

    Update TriggDelBan.sql

--- TriggDelBan.sql
+++ TriggDelBan.sql
@@ -1,6 +1,6 @@
 DELIMITER //
 
-CREATE TRIGGER `UsuwanieBanu` AFTER DELETE ON `ban`
+CREATE TRIGGER `DeleteBan` AFTER DELETE ON `ban`
  FOR EACH ROW BEGIN
 
 SET @text = (SELECT CONCAT('USUNIETO BAN TYPU ', OLD.ban_type ,' O ID ' , OLD.ban_id, ' Z UZYTKOWNIKA O ID ',OLD.user_id,' ZACZYNAJACY SIE ',old.date_since ,' KONCZACY SIE ' ,old.date_by, ' NAŁOŻONY PRZEZ ADMINA O ID ' , old.admin_id));
