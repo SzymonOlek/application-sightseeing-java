abc6ee94216dfb41fc603edfe9f8b2978f71deb8
Szymon <46461910+SzymonOlek@users.noreply.github.com>
2019-11-16 20:27:58 +0100

    Update dodawanieBanu.sql

--- dodawanieBanu.sql
+++ dodawanieBanu.sql
@@ -2,7 +2,7 @@ DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `AddBannedUser`(IN `id_user` INT, IN `adminID` INT, IN `banType` ENUM('comment ban','perma ban'), IN `howManyDays` INT)
 BEGIN
 
-SET @banID = (SELECT max(ban.ban_id) from ban);
+SET @banID = (SELECT max(ban.ban_id)+1 from ban);
 
 IF @banID IS NULL THEN
 SET @banID=1;
