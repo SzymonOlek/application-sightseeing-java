49d2a7dc488d510bc4c644334b4536cc4dd57fa6
PawMid <40070612+PawMid@users.noreply.github.com>
2020-01-03 11:52:57 +0100

    Add files via upload

--- src/main/java/com/project/sightseeing/User/User.java
+++ src/main/java/com/project/sightseeing/User/User.java
@@ -1,5 +1,13 @@
 package com.project.sightseeing.User;
 
+import java.util.Map;
+
+import org.springframework.web.context.request.RequestContextHolder;
+
+import com.project.sightseeing.SightseeingApplication;
+import com.project.sightseeing.Admin.Admin;
+import com.project.sightseeing.Sysuser.Sysuser;
+
 public class User {
 	
 	private String login;
@@ -13,6 +21,22 @@ public class User {
 		this.passwd = null;
 	}
 
+	public String isLogged() {
+		
+		String session = RequestContextHolder.currentRequestAttributes().getSessionId();
+		
+		for (Map.Entry<String, User> entry : SightseeingApplication.loggedInUsers.entrySet()) {
+			if (entry.getKey().equals(session)) {
+				if (entry.getValue() instanceof Admin) {
+					return "a";
+				} else if (entry.getValue() instanceof Sysuser) {
+					return "u";
+				}
+			}
+		}
+		return "g";
+	}
+	
 	public void setLogin(String login) {
 		this.login = login;
 	}
